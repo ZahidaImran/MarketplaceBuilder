9d1ce4f215145596a38b3cc2b2925f21f670b530
Zahida Imran <izahida821@gmail.com>
2025-01-22 11:52:54 +0500

    dynamic

--- src/app/component/productGrid.tsx
+++ src/app/component/productGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductCard from './productcard';
-import IProduct from '@/app/types/types';
+import { IProduct } from '../types/types';
 
 interface ProductsGridProps {
   products: IProduct[];
@@ -10,7 +10,7 @@ const ProductsGrid: React.FC<ProductsGridProps> = ({ products }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard key={product._id} product={product} />
       ))}
     </div>
   );
